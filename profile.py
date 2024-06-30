b71131c5ff0492bbc187225f00184763a28543f6
Liliya9307 <122142141+Liliya9307@users.noreply.github.com>
2024-06-30 17:53:44 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,9 +1,8 @@
 import geni.portal as portal
 import geni.rspec.pg as rspec
 
-# Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
-# Create a XenVM
+
 node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 node.routable_control_ip = "true"
