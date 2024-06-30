836df9fffbaca8340e0b8712e531ec3501843a9f
Liliya9307 <122142141+Liliya9307@users.noreply.github.com>
2024-06-30 17:53:16 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,7 +3,6 @@
 # Path to monitor log file
 MONITOR_LOG="/var/webserver_monitor/unauthorized.log"
 
-# Check if there are new entries
 if [ -s "$MONITOR_LOG" ]; then
     # Send email with log content
     mail -s "Unauthorized Access Detected" your_email@example.com < "$MONITOR_LOG"
