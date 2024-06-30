5258eef07da34e425da028d3322b6449b1c2247c
Liliya9307 <122142141+Liliya9307@users.noreply.github.com>
2024-06-30 11:17:02 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -3,14 +3,24 @@ import geni.rspec.pg as rspec
 
 # Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
-# Create a XenVM
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
-node.routable_control_ip = "true"
 
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
+
+# Create nodes
+for i in range(2):
+    if i == 0:
+        node = request.XenVM("webserver")
+        node.routable_control_ip = "true"  # Ensure webserver has a public IP
+    else:
+        node = request.XenVM("observer")
+        node.routable_control_ip = "false"  # Ensure observer does not have a public IP
+    
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
+    iface = node.addInterface("if" + str(i))
+    iface.component_id = "eth1"
+    iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+    link.addInterface(iface)
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
